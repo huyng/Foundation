a9715f6f5f24fecfc682864f4747ce858ee30d93
huy <huy@huyng.com>
2011-01-21 08:33:20 -0800

    working self extracting unpacker

--- datafiles/packer.sh
+++ datafiles/packer.sh
@@ -7,13 +7,21 @@
 #
 # Bourne shell loader:
 PYTHON=$(which python 2>/dev/null)
+FDN=$(which fdn 2>/dev/null)
 if [ ! -x "$PYTHON" ] ; then
     echo "Python not found!"
     exit 1
 fi
+if [ ! -x "$FDN" ]; then
+    echo "fdn not found, installing ..."
+    git clone git://github.com/huyng/Foundation.git
+    pushd Foundation
+    exec $PYTHON setup.py install --user
+fi
 exec $PYTHON -c "
 # Python loader:
 import sys, os
+import subprocess as SP
 if int(sys.version[0])<2:
     print 'Python version 2.3 final or greater is required.'
     print 'Your version is', sys.version
@@ -26,11 +34,7 @@ if (major==2 and minor<3) or (major==2 and minor==3 and releaselevel!='final'):
     print 'Your version is', sys.version
     os._exit(1)
 fname = sys.argv[1]
-import zipfile
-print sys.argv
-zf = zipfile.ZipFile(fname)
-zf.extractall('/tmp/')
-
+SP.call('fdn add %s' % fname, shell=True)
 " $0 $@
 # Zip file:
 ## end of http://code.activestate.com/recipes/497000/ }}}
